9342eb65eeb8c484072de1622dcd44eaed501cbe
mwhatters <mwhatters@gmail.com>
2015-08-04 00:23:37 -0700

    first-commit

---
+++ index.js
@@ -0,0 +1,43 @@
+var io = require('socket.io').listen(5001)
+var redis = require('redis')
+var commentClient = redis.createClient();
+var roomClient    = redis.createClient();
+
+
+
+// var url = require('url');
+// var redisURL = url.parse(process.env.REDISCLOUD_URL);
+
+// var commentClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+// client.auth(redisURL.auth.split(":")[1]);
+
+// var roomClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+// client.auth(redisURL.auth.split(":")[1]);
+
+
+commentClient.subscribe('comment-created')
+roomClient.subscribe('room-created')
+
+io.sockets.setMaxListeners(20)
+io.on('connection', function(socket) {
+	console.log('connection made')
+});
+
+	commentClient.on('message', function(channel, commentList){
+		console.log('comments recieved')
+		var data = JSON.parse(commentList)
+		
+		var comments = data.slice(1)
+		var room = data[0].url
+		io.sockets.emit(room, comments)
+		}
+	);
+
+	roomClient.on('message', function(channel, roomList) {
+		console.log('room-created message recieved')
+
+		var data = JSON.parse(roomList)
+		io.sockets.emit('room-created', data);
+		}
+	);
+

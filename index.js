635b55ab17425c8ae1b11134893359d14a1aeaf3
mwhatters <mwhatters@gmail.com>
2015-08-04 14:07:00 -0700

    heroku prepare

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-var io = require('socket.io').listen(5001)
+var io = require('socket.io').listen(parseInt(process.env.PORT));
 var redis = require('redis')
-var commentClient = redis.createClient();
-var roomClient    = redis.createClient();
+// var commentClient = redis.createClient();
+// var roomClient    = redis.createClient();
 
 
 
-// var url = require('url');
-// var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var url = require('url');
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
 
-// var commentClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-// client.auth(redisURL.auth.split(":")[1]);
+var commentClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+commentClient.auth(redisURL.auth.split(":")[1]);
 
-// var roomClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-// client.auth(redisURL.auth.split(":")[1]);
-
+var roomClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+roomClient.auth(redisURL.auth.split(":")[1]);
 
 
 commentClient.subscribe('comment-created')
 roomClient.subscribe('room-created')
 
 io.sockets.setMaxListeners(20)
+
+
+// io.configure(function () { 
+//   io.set("transports", ["xhr-polling"]); 
+//   io.set("polling duration", 10); 
+// });
+
 io.on('connection', function(socket) {
 	console.log('connection made')
 });
+
 
 	commentClient.on('message', function(channel, commentList){
 		console.log('comments recieved')
